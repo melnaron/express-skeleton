e18c2cf8f69db636cbb6379aab1d8f3a566a159e
Dmitry Gureev <melnaron@gmail.com>
2013-02-11 13:23:01 +0700

    added controllers autoloading

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 // Load modules
 var express = require('express'),
-	engine  = require('ejs-locals');
+	fs 		= require('fs'),
+	path 	= require('path');
 
 // Create Express app object
 var app = express();
-
-// Create an array of used controllers
-var controllers = ['main', 'test'];
 
 // Setup global variables for responses
 app.locals({
@@ -15,7 +13,7 @@ app.locals({
 });
 
 // Setup Express template engine
-app.engine('html', engine);
+app.engine('html', require('ejs-locals'));
 
 // Setup Express view options
 app.set('views', __dirname + '/views');
@@ -28,22 +26,39 @@ app.use(express.methodOverride());
 app.use(express.static(__dirname + '/public'));
 
 // Initialize controllers
-controllers.forEach(function(controller) {
-	require('./controllers/'+controller)(app);
-});
+(function requireControllers(dir) {
+	fs.readdirSync(dir).forEach(function(file) {
+		// Do not process system files or dirs (started with ".")
+		if (file.match(/^\./)) {
+			return;
+		}
+
+		// Get stats for each file
+		var stats = fs.statSync(path.join(dir, file));
+
+		if (stats.isFile()) {
+			// Require controller
+			require(path.join(dir, file))(app);
+		}
+		else if (stats.isDirectory()) {
+			// Recursive require controllers in sub-directories
+			requireControllers(path.join(dir, file));
+		}
+	});
+}(__dirname + '/controllers'));
 
 // Setup 404 errors middleware
 app.use(function(req, res, next) {
-	console.log('--> Error: 404 Not Found ('+req.originalUrl+')');
-	res.status(404).send('--> Error: 404 Not Found ('+req.originalUrl+')');
+	console.log('--> Error: 404 Not Found ('+req.url+')');
+	res.status(404).send('--> Error: 404 Not Found ('+req.url+')');
 });
 
 // Setup 500 errors middleware
 //app.use(express.errorHandler({ dumpExceptions: false, showStack: false }));
 app.use(function(err, req, res, next) {
-	console.log('--> Error: 500 Internal Server Error ('+req.originalUrl+')');
-	console.log(err.stack);
-	res.status(500).send('--> Error: 500 Internal Server Error ('+req.originalUrl+')');
+	console.error('--> Error: 500 Internal Server Error ('+req.url+')');
+	console.error(err.stack);
+	res.status(500).send('--> Error: 500 Internal Server Error ('+req.url+')');
 });
 
 // Setup graceful shutdown handler
