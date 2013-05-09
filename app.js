a89b767046c8a8326480eae2b75132566fe8e907
Dmitry Gureev <melnaron@gmail.com>
2013-05-09 14:57:12 +0400

    some changes in app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var app = express();
 
 // Setup global variables for responses
 app.locals({
-	title: 'Basic Express skeleton app',
-	year: (new Date).getFullYear().toString()
+	title: 	'Basic Express skeleton app',
+	year: 	(new Date).getFullYear().toString()
 });
 
 // Setup Express template engine
@@ -19,6 +19,9 @@ app.engine('html', require('ejs-locals'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
+// Disable 'X-Powered-By' header expose
+//app.disable('x-powered-by');
+
 // Setup Express middlewares
 app.use(express.bodyParser());
 app.use(express.cookieParser());
@@ -26,7 +29,7 @@ app.use(express.methodOverride());
 app.use(express.static(__dirname + '/public'));
 
 // Initialize controllers
-(function requireControllers(dir) {
+(function requireControllers(dir, params) {
 	fs.readdirSync(dir).forEach(function(file) {
 		// Do not process system files or dirs (started with ".")
 		if (file.match(/^\./)) {
@@ -38,14 +41,14 @@ app.use(express.static(__dirname + '/public'));
 
 		if (stats.isFile()) {
 			// Require controller
-			require(path.join(dir, file))(app);
+			require(path.join(dir, file)).apply(null, params);
 		}
 		else if (stats.isDirectory()) {
 			// Recursive require controllers in sub-directories
-			requireControllers(path.join(dir, file));
+			requireControllers(path.join(dir, file), params);
 		}
 	});
-}(__dirname + '/controllers'));
+})(__dirname + '/controllers', [ app ]);
 
 // Setup 404 errors middleware
 app.use(function(req, res, next) {
@@ -56,21 +59,39 @@ app.use(function(req, res, next) {
 // Setup 500 errors middleware
 //app.use(express.errorHandler({ dumpExceptions: false, showStack: false }));
 app.use(function(err, req, res, next) {
-	console.error('--> Error: 500 Internal Server Error ('+req.url+')');
-	console.error(err.stack);
-	res.status(500).send('--> Error: 500 Internal Server Error ('+req.url+')');
+	console.error('--> Error: 500 Internal Server Error ('+req.url+'): '+(err.message || err));
+	if (err.stack) {
+		console.error(err.stack);
+	}
+	res.status(500).send('--> Error: 500 Internal Server Error ('+req.url+'): '+(err.message || err));
 });
 
 // Setup graceful shutdown handler
 function onShutdown() {
-	console.log('');
 	console.log('Exiting...');
 	// Some shutdown logic here...
 	// ...
 	process.exit();
 }
-process.on('SIGINT',  onShutdown); // CTRL+C
-process.on('SIGTERM', onShutdown); // kill -15 <pid>
+
+// Listen for SIGINT (CTRL+C) signal to properly stop the app
+process.on('SIGINT', function() {
+	console.log('\nSIGINT signal received');
+	onShutdown();
+});
+
+// Listen for SIGTERM (kill -15 <pid>) signal to properly stop the app
+process.on('SIGTERM', function() {
+	console.log('\nSIGTERM signal received');
+	onShutdown();
+});
+
+// Listen for 'uncaughtException' event
+process.on('uncaughtException', function(err) {
+	console.error('Exception thrown', err);
+	console.error(err.stack);
+	process.exit(1);
+});
 
 // Listen for requests
 app.listen(7001);
